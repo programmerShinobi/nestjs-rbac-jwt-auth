7fdf5548b44473809231b9cf7dc7fefbc9d6a86e
programmerShinobi <shinobiprogrammer@gmail.com>
2024-11-09 22:50:02 +0700

    [add] 1st series : + Add initial project files and configuration + Add User as a resource + Add .env file to gitignore + Update docker-compose.yml to configure MongoDB container + Update npm dependencies and add MongoDB configuration + Add User entity and module with Mongoose integration

---
+++ src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // opsional, agar ConfigModule bisa diakses secara global
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+    UsersModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
